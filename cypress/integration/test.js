03ffeba16e20eb48c4e74298623a6b73fdf27ba2
fabcaironi72 <71944665+fabcaironi72@users.noreply.github.com>
2020-09-26 21:58:59 -0300

    agregado el log con el número de resultados

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -8,12 +8,14 @@ context('eBay Test', () => {
     it('Search for Pilas and print the number of items found to the console', function () {
         //   Completamos el campo de búsqueda con la palabra "Pilas"
         cy.get('#gh-ac').type('Pilas')
+        cy.wait(2000)
         //   Hacemos click en la lupa
         cy.get('#gh-btn').click()
+
         // Imprimimos en consola sólo el número de resultados encontrados
         cy.get(".srp-controls__count-heading").invoke('text').then((text) => {
-           var itemsFound = text.split(" ");
-           cy.log(itemsFound[0])
+            var arrText = text.split(" ");
+            cy.log('NÚMERO DE ITEMS DEVUELTO: '+arrText[0])
         });
     })
 })
